21800c3e89741e72fb7c147fa704bde408e6dac2
Marc <marc.schwendemann@lernende.bbw.ch>
2021-05-26 11:43:16 +0200

    Implement Calculator Class.

---
+++ src/main/java/ch/bbw/ms/consolecalculator/Calculator.java
@@ -0,0 +1,15 @@
+package ch.bbw.ms.consolecalculator;
+
+/**
+ * Calculator
+ *  - kann zwei Zahlen addieren
+ *
+ * @author Marc Schwendemann
+ * @version 19.05.2021
+ */
+
+public class Calculator {
+    public int summe(int summand1, int summand2) {
+        return summand1 + summand2;
+    }
+}
