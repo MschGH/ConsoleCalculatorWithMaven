0dd4424fee210e28253465910c36cefea940e742
Marc <marc.schwendemann@lernende.bbw.ch>
2021-06-23 11:35:09 +0200

    Implement setup for tests.

--- src/test/java/ch/bbw/ms/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/ms/consolecalculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package ch.bbw.ms.consolecalculator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,27 +8,28 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
     Calculator testee;
 
+    @Before
+    public void setUp() {
+        testee = new Calculator();
+    }
+
     @Test
     public void testSummeZweiPositiveIsOk() {
-        testee = new Calculator();
         assertTrue(testee.summe(10, 25) == 35);
     }
 
     @Test
     public void testSummePositivUndNegativIsOk() {
-        testee = new Calculator();
         assertTrue(testee.summe(30, -10) == 20);
     }
 
     @Test
     public void testSubtractionZweiPositiveIsOk() {
-        testee = new Calculator();
         assertTrue(testee.subtraction(25, 10) == 15);
     }
 
     @Test
     public void testSubtractionPositivUndNegativIsOk() {
-        testee = new Calculator();
         assertTrue(testee.subtraction(15, -10) == 25);
     }
 }
